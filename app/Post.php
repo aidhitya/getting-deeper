a0a61f0f7278c817cefacd7978786a322d14b2ca
pngdhty <pngdhty@users.github.com>
2020-08-14 15:07:37 +0700

    Polymorphic => One to Many

--- app/Post.php
+++ app/Post.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Image;
+use App\Comment;
 
 class Post extends Model
 {
@@ -12,5 +13,10 @@ class Post extends Model
     public function image()
     {
         return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
     }
 }
--- database/migrations/2020_08_14_074744_create_comments_table.php
+++ database/migrations/2020_08_14_074744_create_comments_table.php
@@ -15,6 +15,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('body');
+            $table->unsignedBigInteger('commentable_id');
+            $table->string('commentable_type');
             $table->timestamps();
         });
     }
