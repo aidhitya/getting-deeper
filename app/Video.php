e037301fccac110bd24463478ab8a5ecfcbd4cd4
pngdhty <pngdhty@users.github.com>
2020-08-14 15:15:35 +0700

    Polymorphic => One to Many (2)

---
+++ app/Video.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Comment;
+
+class Video extends Model
+{
+    protected $guarded = [];
+
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+}
